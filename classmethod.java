5ef063fc645b34f8d118eb5e8cf3d0f04c43b3b9
Patrick Ojunde <patrickojunde@gmail.com>
2019-12-03 19:05:53 +0100

    JavaCode

---
+++ classmethod.java
@@ -0,0 +1,8 @@
+public class Account{
+	public int withdraw (int pin){
+	}
+	public void deposit (int amount, int accountno){
+	}
+	public int transfer (int amount, int pin, int transferaccount){
+	}
+}
