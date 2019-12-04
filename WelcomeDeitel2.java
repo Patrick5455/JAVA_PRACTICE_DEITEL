b06b2f9a55f88e7f31a1e28112b27c614c1c0fcb
Patrick Ojunde <patrickojunde@gmail.com>
2019-12-04 07:59:45 +0100

    My Deitel NOtes

---
+++ WelcomeDeitel2.java
@@ -0,0 +1,25 @@
+//Declaring a Method
+public class WelcomeDeitel2 {
+	//main method begins execution of Java application & must be in line 6.			//For a Java application, one of the methods must be called 			//main & must be defined as in line 6; otherwise,d program won't execute.
+
+	public static void main(String[] args) {
+
+		System.out.print("Welcome to "); 
+		System.out.println("Java Programming!");
+
+	} // Unlike println, after displaying its argument, print does not 		//position the output cursor at the beginning of the next line—the next 		//character the program displays will appear immediately after the last 		//character that print displays.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} // end class WelcomeDeitel2
