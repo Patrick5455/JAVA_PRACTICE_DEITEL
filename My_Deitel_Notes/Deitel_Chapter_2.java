269b426dc64e2ec7443ea4a00d0a259e5abe321d
Patrick Ojunde <patrickojunde@gmail.com>
2019-12-06 04:10:38 +0100

    Updated Note

--- My_Deitel_Notes/Deitel_Chapter_2.java
+++ My_Deitel_Notes/Deitel_Chapter_2.java
@@ -1,6 +1,6 @@
 //Declaring a Method
 public class Deitel_Chapter_2 {
-	//main method begins execution of Java app & must be in line 6		
+	//main method begins execution of Java app		
 	//For a Java application, one of the methods must be called			
 	//main & must be defined as in line 6; otherwise,d program won't execute
 	public static void main(String[] args) {
@@ -21,7 +21,22 @@ public class Deitel_Chapter_2 {
 // Displaying Text with printf
 // Method System.out.printf (f means "formatted") displays formatted //data.
 
-	}
+		System.out.printf("%s%n%s%n", "Welcome to", "Java Programming");
+		
+		System.out.printf("%s%n%s%n%s%n", "Patrick", "Welcome to", "Java Programming");
+		
+		System.out.printf("%s%n%s%n%s%n", "The method call specifies three arguments.", "When a method requires multiple arguments, they\'re placed in a comma-separated list.", "Calling a method is also referred to as invoking a method.");
+		
+		/*Method printf’s first argument is a format string that may consist of fixed text and format specifiers. Each format specifier is a placeholder for a value and specifies the type of data to output. Format specifiers also may include optional formatting information.  
+		
+		Format specifiers begin with a percent sign (%) followed by a character that represents the data type.
+		
+		Notice that instead of using the escape sequence \n, we used the %n format specifier, which is a line separator that’s portable across operating systems. You cannot use %n in the argument to System.out.print or System.out.println; however, the line separator output by System.out.println after it displays its argument is portable across operating systems.*/
+		
+//Another Application: Adding Integers
+
+		
+		}
 
 
 
