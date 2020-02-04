8657572de5bae41da5b30b1d81d0805210059d77
Patrick OJunde <patrickojunde@gmail.com>
2020-02-04 03:26:53 +0100

    Adding an ArrayList

--- randomAlgorithm/AddArrayList.java
+++ randomAlgorithm/AddArrayList.java
@@ -3,10 +3,15 @@ package randomAlgorithm;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class AddArrayList {
+
     public static void main (String... args){
+
         ArrayList <Integer> arr = new ArrayList<Integer> ();
+
         Scanner input = new Scanner(System.in);
         int total =0; int counter=0;
+
+        System.out.println("Enter a set of  numbers to sum. To stop entry, press ctrl+D on Linux and Ctrl+W on Windows");
         while (input.hasNext()){
             int num = input.nextInt();
             arr.add(num);
@@ -14,9 +19,5 @@ public class AddArrayList {
             counter++;
         }
         System.out.printf("Total is %d",total);
-
-
-
-
     }
 }
