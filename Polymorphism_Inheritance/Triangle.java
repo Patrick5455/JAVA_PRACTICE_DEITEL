e42cbcb3dafd310dc1b8c3dd294722141c270817
Patrick OJunde <patrickojunde@gmail.com>
2020-02-09 22:15:16 +0100

    Practice on Polymorpism and Inheritance

--- Polymorphism_Inheritance/Triangle.java
+++ Polymorphism_Inheritance/Triangle.java
@@ -1,6 +1,6 @@
 package Polymorphism_Inheritance;
 
-public class Triangle extends Polygon_Inheritance  { 
+public class Triangle extends Polygon_Inheritance  {
 
     public int area () {
         return (length * height) / 2;
