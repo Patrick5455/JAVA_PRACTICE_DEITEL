d53682a855822758c7edd1801d1927092e607043
Patrick5455 <patrickojunde@gmail.com>
2020-06-03 08:49:45 +0100

    Create Package Advanced Concepts

---
+++ some-advanced-java-conepts/src/advancedConcepts/Inheritance.java
@@ -0,0 +1,16 @@
+package advancedConcepts;
+
+public class Inheritance {
+
+/* Classes in Java can be extended
+*  creating new classes which retain
+* characteristics of the base class
+* */
+
+    public static void main (String... args){
+
+        
+    }
+
+
+}
